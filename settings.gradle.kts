da74f192ae749e0a230cfd60fd36942898e54fdc
Alexander Cacuango <amcacuangoa@uce.edu.ec>
2024-07-24 03:36:27 -0500

    Sin_error

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,10 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
+
+        maven ( url = "https://jitpack.io")
     }
 }
 dependencyResolutionManagement {
@@ -16,8 +12,11 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+
+        maven ( url = "https://jitpack.io")
     }
 }
+
 
 rootProject.name = "Maurovision"
 include(":app")
